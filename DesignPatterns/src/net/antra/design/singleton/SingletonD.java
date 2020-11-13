df6cec1fcc3bba56c1a4be1f3dca54c1d811884b
hensby <whc0721a@outlook.com>
2020-11-12 18:31:47 -0800

    builder

--- DesignPatterns/src/net/antra/design/singleton/SingletonD.java
+++ DesignPatterns/src/net/antra/design/singleton/SingletonD.java
@@ -1,7 +1,7 @@
 package net.antra.design.singleton;
 
 public class SingletonD {
-	private static volatile SingletonD instance= null; // visitable in main memory(让cache里的改变即使存到main memory里）让指令保持顺序
+	private static volatile SingletonD instance= null; // visitable in main memory(让cache里的改变即时存到main memory里）让指令保持顺序
 //	private static String lock = "";
 	private SingletonD(){}
 	public static SingletonD getInstance(){
